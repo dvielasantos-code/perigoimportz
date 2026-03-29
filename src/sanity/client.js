e148c155c885b4ba0f30d337e7313ced4a363ba0
dvielasantos-code <dvielasantos-code@users.noreply.github.com>
2026-03-28 23:14:58 -0300

    fix(sanity): forcing access token to bypass permissions and CORS

--- src/sanity/client.js
+++ src/sanity/client.js
@@ -6,7 +6,8 @@ export const client = createClient({
   projectId,
   dataset,
   useCdn: true, // Use o cache rápido (CDN) pra carregar imagens velozmente
-  apiVersion: '2024-03-28' // Data de hoje
+  apiVersion: '2024-03-28', // Data de hoje
+  token: 'skFNN4TfWgk0fiiof8ES7Gz4ylBbNmi0O4JJG9zF2Zi9QIjC9xpU2yv9Acb0cAxkySqlNgfT4bT8wsLwiNdsjpUQe7qSgIrUMVnzkvWWkqPo0DDuYkUtzOnIXH6tDlYx3vkGKcB4Z4OqtMO9l5UhACrDzpjZgYyGvmjriGqLVC7BueUpYw2S' // Bypass absoluto de permissões
 });
 
 const builder = imageUrlBuilder(client);
